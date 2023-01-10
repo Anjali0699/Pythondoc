8915052861c50c4544b531e766535d8d5ad8d6d5
Anjali0699 <geethanjali1999@gmail.com>
2023-01-10 15:59:55 +0530

    added

---
+++ ecommerce/ecommerceproject/ecommerceapp/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Catergory(models.Model):
+    name = models.CharField(max_length=250,unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    descp = models.TextField(blank=True)
+    img = models.ImageField(upload_to='category',blank=True)
+
+    class Meta:
+        ordering=('name',)
+        verbose_name='category'
+        verbose_name_plural='categories'
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+    def get_url(self):
+        return reverse('ecommerceapp:products_by_category',args=[self.slug])
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    descp = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    category = models.ForeignKey(Catergory,on_delete=models.CASCADE)
+    img = models.ImageField(upload_to='products', blank=True)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def get_url(self):
+        return reverse('ecommerceapp:prodCatdetail', args=[self.category.slug,self.slug])
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return '{}'.format(self.name)
